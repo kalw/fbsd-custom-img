d161e9fff1ef486dc8a476adb0e421fed537f6c8
kalw <regis.despres@gmail.com>
2025-10-24 02:22:14 +0200

    fix: sh compliance

--- make.iso.with.installer.sh
+++ make.iso.with.installer.sh
@@ -5,18 +5,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # the temp directory used, within $DIR
 # omit the -p parameter to create a temporal directory in the default location
 WORK_DIR=`mktemp -d -p "$DIR"`
+
 # check if tmp dir was created
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+if [ ! "$WORK_DIR" ] || [ ! -d "$WORK_DIR" ]; then
   echo "Could not create temp dir"
   exit 1
 fi
 # deletes the temp directory
-function cleanup {      
+cleanup() {
   rm -rf "$WORK_DIR"
   echo "Deleted temp working directory $WORK_DIR"
 }
 # register the cleanup function to be called on the EXIT signal
-trap cleanup EXIT
+trap 'cleanup' 0
 
 pkg install -y curl
 
@@ -44,7 +45,7 @@ mdconfig -du 0
 
 mkdir -p artifacts
 mv ${FREEBSD_ISO_DL_PATH}/${FREEBSD_IMG_NAME} artifacts/${FREEBSD_FLAVOR}-${FREEBSD_IMG_NAME}
-if [ "$CIRRUS_BRANCH" == "main" ]; then
+if [ "$CIRRUS_BRANCH" = "main" ]; then
   echo "releasing"
   pkg install -y gh cocogitto
   git config --global user.name "release-bot"
