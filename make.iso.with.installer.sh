f7050422b0488beed4186c5c60393dda46b9236a
kalw <regis.despres@gmail.com>
2025-10-31 20:26:30 +0100

    chore: simplify

--- make.iso.with.installer.sh
+++ make.iso.with.installer.sh
@@ -55,7 +55,7 @@ for FREEBSD_ARCH  in $(echo ${FREEBSD_ARCHS} | tr ',' ' '); do
     echo "Preparing FreeBSD version: ${FREEBSD_VERSION}"
     FREEBSD_IMG_NAME="FreeBSD-${FREEBSD_VERSION}-RELEASE-$(echo ${FREEBSD_ARCH} |sed -e 's/\//-/')-mini-memstick.img"
     FREEBSD_ISO_URL="https://download.freebsd.org/releases/${FREEBSD_ARCH}/ISO-IMAGES/${FREEBSD_VERSION}/${FREEBSD_IMG_NAME}"
-    fetch -o ${FREEBSD_ISO_DL_PATH}/${FREEBSD_IMG_NAME} "${FREEBSD_ISO_URL}" 
+    curl -o ${FREEBSD_ISO_DL_PATH}/${FREEBSD_IMG_NAME} "${FREEBSD_ISO_URL}" 
     
     for FREEBSD_FLAVOR in ${FREEBSD_FLAVORS | tr ',' ' '}; do
       mdconfig -u 0 -f ${FREEBSD_ISO_DL_PATH}/${FREEBSD_IMG_NAME}
